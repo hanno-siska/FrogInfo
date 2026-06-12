a7a0e5b40c829d9d95a89065c85c01132f4657c3
Hanno Siska <Hanno.siska2@gmail.com>
2026-06-12 19:38:03 +0300

    Finish /contact, its very nice

--- app/views/contact.php
+++ app/views/contact.php
@@ -4,6 +4,10 @@ $content = "contact";
 include __DIR__."/../static/templates/page_start.php";
 ?>
 
+<h2>Contact</h2>
+<p>Yea no, we don't do contact forms here.</p>
+<p>If you need something, imagine sending an email to a frog.</p>
+
 <?php
 include __DIR__."/../static/templates/page_end.php";
 ?>
